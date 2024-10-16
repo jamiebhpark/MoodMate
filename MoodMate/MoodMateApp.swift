df2ac37e4cb32801867f978addd36c897f5ec731
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-10-16 11:42:17 +0900

    Initial Commit

---
+++ MoodMate/MoodMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMateApp.swift
+//  MoodMate
+//
+//  Created by JONGHUN PARK on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
